48da94fdded28913b7e63c47081f2295bb685c81
Vincent Dubois <dubois.vct@free.fr>
2016-12-04 15:09:06 +0100

    [FIX] Observable.zip with 2 service calls

--- src/main/java/io/github/vdubois/model/Book.java
+++ src/main/java/io/github/vdubois/model/Book.java
@@ -1,5 +1,7 @@
 package io.github.vdubois.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -29,6 +31,7 @@ public class Book {
     @NotNull
     private Date publicationDate;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "books_authors", joinColumns = {
             @JoinColumn(name = "book_id", nullable = false, updatable = false)
