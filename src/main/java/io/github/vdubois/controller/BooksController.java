8de70715aadaa456e5957d91e4e22f603b78e77b
Vincent Dubois <dubois.vct@free.fr>
2016-12-18 19:41:54 +0100

    [FIX] Better handling of error 404

--- src/main/java/io/github/vdubois/controller/BooksController.java
+++ src/main/java/io/github/vdubois/controller/BooksController.java
@@ -2,9 +2,13 @@ package io.github.vdubois.controller;
 
 import io.github.vdubois.model.Book;
 import io.github.vdubois.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * Created by vdubois on 03/12/16.
@@ -19,7 +23,10 @@ public class BooksController {
     }
 
     @GetMapping("/books/{isbn}")
-    public Book findBookByIsbn(@PathVariable("isbn") String isbn) {
-        return bookService.findOneByIsbn(isbn);
+    public ResponseEntity<Book> findBookByIsbn(@PathVariable("isbn") String isbn) {
+        Book book = bookService.findOneByIsbn(isbn);
+        return Optional.ofNullable(book)
+                .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
